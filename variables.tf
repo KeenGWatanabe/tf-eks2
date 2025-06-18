d967c56aa1db578f221c30c1b70cddcd48b09369
KeenGWatanabe <mckeenweb@gmail.com>
2025-06-19 06:59:55 +0800

    thunder

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "name_prefix" {
   description = "Prefix for resource names"
   type        = string
-  default     = "custom" # Change to your preferred prefix  
+  default     = "thunder" # Change to your preferred prefix  
 }
 variable "cluster_name" {
   description = "EKS Cluster name"
   type        = string
-  default     = "custom-cluster" # Change to your preferred cluster name
+  default     = "thunder-cluster" # Change to your preferred cluster name
 }
